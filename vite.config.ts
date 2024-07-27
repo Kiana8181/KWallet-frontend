e5b458f0034e2fcdcf7af4b654bb53b45c646e2e
Kiana8181 <kianahashemi8@gmail.com>
2024-07-28 01:56:03 +0330

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/kwallet/",
   plugins: [react()],
   server: {
     host: "0.0.0.0", // This allows external connections
